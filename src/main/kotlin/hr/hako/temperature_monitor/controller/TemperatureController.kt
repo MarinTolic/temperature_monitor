43d70be54aea376f2f1c8968a54bf3bca5a64df7
macpro <marin.t91@gmail.com>
2021-04-25 14:32:44 +0200

    - created a websocket configuration and a controller

--- src/main/kotlin/hr/hako/temperature_monitor/controller/TemperatureController.kt
+++ src/main/kotlin/hr/hako/temperature_monitor/controller/TemperatureController.kt
@@ -1,7 +1,21 @@
 package hr.hako.temperature_monitor.controller
 
+import hr.hako.temperature_monitor.model.controller_models.CombinedReading
+import hr.hako.temperature_monitor.persistence.interactors.ReadingInteractor
+import hr.hako.temperature_monitor.persistence.repository.HumidityRepository
+import hr.hako.temperature_monitor.persistence.repository.TemperatureRepository
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.stereotype.Controller
 
 @Controller
-class TemperatureController {
+class TemperatureController(private val readingInteractor: ReadingInteractor) {
+
+    @MessageMapping("/combinedReading")
+    fun readingMapping(combinedReading: CombinedReading){
+        readingInteractor.tryEmitReading(combinedReading)
+    }
+
 }
