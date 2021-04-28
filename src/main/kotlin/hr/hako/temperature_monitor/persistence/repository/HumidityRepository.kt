bcc1fa68def94f6f199f9d4a48318494aa42c730
macpro <marin.t91@gmail.com>
2021-04-28 18:23:58 +0200

    - changed repositories to interfaces instead of abstract class so Spring could generate the necessary code

--- src/main/kotlin/hr/hako/temperature_monitor/persistence/repository/HumidityRepository.kt
+++ src/main/kotlin/hr/hako/temperature_monitor/persistence/repository/HumidityRepository.kt
@@ -3,5 +3,5 @@ package hr.hako.temperature_monitor.persistence.repository
 import hr.hako.temperature_monitor.model.entities.HumidityReading
 import org.springframework.data.jpa.repository.JpaRepository
 
-abstract class HumidityRepository : JpaRepository<HumidityReading, Long> {
+interface HumidityRepository : JpaRepository<HumidityReading, Long> {
 }
